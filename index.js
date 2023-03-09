a2e10cf316a32af7c3e6e9b0008a96764f41e52c
BogAlex4ndr <bog.alex4ndr@gmail.com>
2023-03-09 13:22:13 +0200

    changes uploads

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if(fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads')
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
